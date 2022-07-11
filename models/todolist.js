1eb449bc734dece5ba9e09a8cf78d90c71520ea6
Miriamvdb <miriamvdb@live.nl>
2022-07-11 20:32:11 +0200

    Setup relations

--- models/todolist.js
+++ models/todolist.js
@@ -8,7 +8,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      todoList.belongsTo(models.user, { foreignKey: "userId" });
+      todoList.hasMany(models.todoItem, { foreignKey: "listId" });
     }
   }
   todoList.init(
