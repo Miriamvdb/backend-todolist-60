9854e478e5b0e80829d3d58df32f6d3567042220
Miriamvdb <miriamvdb@live.nl>
2022-07-11 14:05:04 +0200

    Generated models / migrations / seeders + new column

---
+++ seeders/20220711114353-todoLists.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "todoLists",
+      [
+        {
+          name: "Girls I wanna date",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          // userId: 1,
+        },
+        {
+          name: "Things I wanna do now I've left my friends",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          // userId: 2,
+        },
+        {
+          name: "Eleminate all the back-players",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          // userId: 3,
+        },
+        {
+          name: "Boys I wanna kiss",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          // userId: 4,
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("todoLists", null, {});
+  },
+};
